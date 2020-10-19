873db439e6fb8def0bdc758bda2b794844c7ec3b
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2020-10-19 16:15:15 +0700

    [UPDATE] update Clg

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const port =  process.env.PORT || 3000;
 app.get("/data", (req, res) => {
   console.log("success!");
   console.log("test github");
-  console.log("github");
 //   console.log(req);
   res.json({ ok: "success" });
 });
